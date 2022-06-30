86ee91486ac01ea90b871b015fbd9a0383d5c691
admy0101 <a.d.myers1986@gmail.com>
2022-06-30 12:55:45 +0100

    start to style login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,43 @@
 import { useState, useContext } from "react";
 import { userContext } from "../Context/User.js";
+import styles from "../styles/login.module.css";
+
 
 export const Login = () => {
   const { setUser } = useContext(userContext);
-  const [userToBuild, setUserToBuild] = useState({ username: "" });
+  const [userToBuild, setUserToBuild] = useState({
+    username: "",
+    password: "",
+  });
+  const validUser = [
+    { username: "nathan", password: "123" },
+    { username: "andy", password: "456" },
+  ];
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setUser((oldUser) => {
-      const newUser = { ...oldUser };
-      newUser.username = userToBuild.username;
-      return newUser;
+    const filteredUsers = validUser.filter((user) => {
+      return userToBuild.username === user.username;
     });
-    setUserToBuild({ username: "" });
+    if (
+      filteredUsers.length === 1 &&
+      filteredUsers[0].password === userToBuild.password
+    ) {
+      setUser((oldUser) => {
+        const newUser = { ...oldUser };
+        newUser.username = userToBuild.username;
+        return newUser;
+      });
+    } else {
+      alert("Please enter valid username or password");
+    }
+
+    setUserToBuild({ username: "", password: "" });
   };
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className={styles.loginCard}>
+        <div className={styles.formElement}>
+        <label>Username </label>
       <input
         type="text"
         value={userToBuild.username}
@@ -27,6 +49,21 @@ export const Login = () => {
           });
         }}
       ></input>
+      </div>
+      <div className={styles.formElement}>
+      <label>Password </label>
+      <input
+        type="password"
+        value={userToBuild.password}
+        onChange={(event) => {
+          setUserToBuild((oldUser) => {
+            const newUser = { ...oldUser };
+            newUser.password = event.target.value;
+            return newUser;
+          });
+        }}
+      ></input>
+      </div>
       <button>Submit</button>
     </form>
   );
