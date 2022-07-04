a50e5e3c99f59a676151ffcc670ea6099409f3a6
alousha <itsaliabdallah@gmail.com>
2022-07-04 20:09:16 +0100

    login changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { userContext } from "../Context/User.js";
 import styles from "../styles/login.module.css";
 import { loginUser, getProfile } from "../utils/api.js";
